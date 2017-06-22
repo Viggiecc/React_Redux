e793428b128fa85c0f486b221aed7239cc4355c5
Viggiecc <vigiecc@gmail.com>
2017-06-22 15:27:13 -0700

    New Container Weather List

--- src/reducers/weather_reducer.js
+++ src/reducers/weather_reducer.js
@@ -5,11 +5,9 @@ export function WeatherReducer(state = [], action) {
   switch(action.type) {
     case FETCH_WEATHER:
       //ES6 sytax
-      console.log('hhhh');
-      console.log('state:', [action.payload.data, ...state]);
       return [action.payload.data, ...state];
       //ES5 both return a new array, not mutate the existing state array
-      return state.concat([action.payload.data]);
+      // return state.concat([action.payload.data]);
   }
   return state;
 }
