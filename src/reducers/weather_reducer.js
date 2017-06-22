9b0be2c8e2081901374d9a99eb26fe1618b7dccc
Viggiecc <vigiecc@gmail.com>
2017-06-22 14:29:18 -0700

    Perfect Weather Reducer

--- src/reducers/weather_reducer.js
+++ src/reducers/weather_reducer.js
@@ -1,4 +1,15 @@
-export function WeatherReducer(state = null, action) {
-  console.log('Action: ', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export function WeatherReducer(state = [], action) {
+  console.log('Action:', action);
+  switch(action.type) {
+    case FETCH_WEATHER:
+      //ES6 sytax
+      console.log('hhhh');
+      console.log('state:', [action.payload.data, ...state]);
+      return [action.payload.data, ...state];
+      //ES5 both return a new array, not mutate the existing state array
+      return state.concat([action.payload.data]);
+  }
   return state;
 }
